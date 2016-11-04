a18d45658f5831eaa2a0c8a329da98d22bc1e83e
Marcelo Politzer Couto <mpolitzer.c@gmail.com>
2016-11-03 23:45:44 -0200

    added string_split

--- tz_string-t.c
+++ tz_string-t.c
@@ -1,7 +1,7 @@
 #define TZ_UNIT_TEST_INLINE
 #include "tz_unit_test.h"
 
-#define TZ_STD_STRING_INLINE
+#define TZ_DS_STRING_INLINE
 #include "tz_string.h"
 
 void check_format()
@@ -26,11 +26,31 @@ void check_toupper()
 	tz_unit_test_should_pass(strncmp(expected, (char *)s.p, s.n) == 0);
 }
 
+void check_split()
+{
+	char *input       = " hello world ";
+	char *expected[2] = {
+		"hello",
+		"world"
+	};
+
+	tz_array strings = tz_string_split(input);
+	tz_string *s0 = tz_array_sub_i(&strings, 0);
+	tz_string *s1 = tz_array_sub_i(&strings, 1);
+
+	//printf("1.|"TZ_STRING_TO_FMT"|\n", tz_string_to_fmt(s0));
+	//printf("2.|"TZ_STRING_TO_FMT"|\n", tz_string_to_fmt(s1));
+	tz_unit_test_should_pass(
+			memcmp(s0->p, expected[0], s0->n) == 0 &&
+			memcmp(s1->p, expected[1], s1->n) == 0);
+}
+
 int main(int argc, char const* argv[])
 {
 	tz_unit_test_start(argv[0]);
 	check_format();
 	check_toupper();
+	check_split();
 //	check_capitalize();
 	return tz_unit_test_report();
 }
--- tz_string.h
+++ tz_string.h
@@ -1,19 +1,19 @@
-#ifndef TZ_STD_STRING_H
-#define TZ_STD_STRING_H
+#ifndef TZ_DS_STRING_H
+#define TZ_DS_STRING_H
 #include <stdarg.h>
 #include <ctype.h>
 /* includes-here */
 
-#ifdef TZ_STD_STRING_INLINE
-#  define TZ_STD_STRING_M static inline
-#  define TZ_STD_STRING_DECLARATIONS
+#ifdef TZ_DS_STRING_INLINE
+#  define TZ_DS_STRING_M static inline
+#  define TZ_DS_STRING_DECLARATIONS
 
-#  ifndef TZ_STD_ARRAY_INLINE
-#    define TZ_STD_ARRAY_INLINE TZ_STD_STRING_INLINE
+#  ifndef TZ_DS_ARRAY_INLINE
+#    define TZ_DS_ARRAY_INLINE TZ_DS_STRING_INLINE
 #  endif
 
 #else
-#  define TZ_STD_STRING_M extern
+#  define TZ_DS_STRING_M extern
 #endif
 #include "tz_array.h"
 /* tz-includes-here */
@@ -29,41 +29,50 @@ typedef tz_array tz_string; // type alias
 #define tz_string_to_fmt(s) (s)->n, (s)->p
 
 /** Create an array by value */
-TZ_STD_STRING_M T    tz_string_create     (void);
-TZ_STD_STRING_M T    tz_string_create_s   (const char *s);
-TZ_STD_STRING_M void tz_string_destroy    (T *me);
-TZ_STD_STRING_M T    tz_string_dup        (T *me);
-TZ_STD_STRING_M T   *tz_string_format     (T *me, const char *fmt, ...);
-TZ_STD_STRING_M T   *tz_string_toupper    (T *me);
-TZ_STD_STRING_M T   *tz_string_toupper_s  (T *me, const char *s);
+TZ_DS_STRING_M T        tz_string_create   (void);
+TZ_DS_STRING_M T        tz_string_create_b (const char *s, size_t n);
+TZ_DS_STRING_M T        tz_string_create_s (const char *s);
+TZ_DS_STRING_M void     tz_string_destroy  (T *me);
+TZ_DS_STRING_M T        tz_string_dup      (T *me);
+TZ_DS_STRING_M T       *tz_string_format   (T *me, const char *fmt, ...);
+TZ_DS_STRING_M T       *tz_string_toupper  (T *me);
+TZ_DS_STRING_M T       *tz_string_toupper_s(T *me, const char *s);
+TZ_DS_STRING_M tz_array tz_string_split    (char *s);
 
-#ifdef TZ_STD_STRING_DECLARATIONS
+#ifdef TZ_DS_STRING_DECLARATIONS
 
-TZ_STD_STRING_M T tz_string_create(void)
+TZ_DS_STRING_M T tz_string_create(void)
 {
 	return tz_array_create(sizeof(char));
 }
 
-TZ_STD_STRING_M T tz_string_create_s(const char *s)
+TZ_DS_STRING_M T tz_string_create_b (const char *s, size_t n)
 {
 	T me = tz_string_create();
-	tz_array_pushn_data(&me, strlen(s), &s);
+	tz_array_pushn_data(&me, n, (void *)s);
 	return me;
 }
 
-TZ_STD_STRING_M void tz_string_destroy  (T *me)
+TZ_DS_STRING_M T tz_string_create_s(const char *s)
+{
+	T me = tz_string_create();
+	tz_array_pushn_data(&me, strlen(s), (void *)s);
+	return me;
+}
+
+TZ_DS_STRING_M void tz_string_destroy  (T *me)
 {
 	tz_array_destroy(me);
 }
 
-TZ_STD_STRING_M T tz_string_dup(T *me)
+TZ_DS_STRING_M T tz_string_dup(T *me)
 {
 	T new = tz_string_create();
 	tz_array_pushn_data(&new, me->n, me->p);
 	return new;
 }
 
-TZ_STD_STRING_M T *tz_string_format(T *me, const char *fmt, ...)
+TZ_DS_STRING_M T *tz_string_format(T *me, const char *fmt, ...)
 {
 	va_list ap;
 	va_start(ap, fmt);
@@ -84,21 +93,21 @@ TZ_STD_STRING_M T *tz_string_format(T *me, const char *fmt, ...)
 	return me;
 }
 
-TZ_STD_STRING_M T *tz_string_toupper(T *me)
+TZ_DS_STRING_M T *tz_string_toupper(T *me)
 {
 	for (uint32_t i=0; i<me->n; ++i)
 		me->p[i] = toupper(me->p[i]);
 	return me;
 }
 
-TZ_STD_STRING_M T *tz_string_tolower(T *me)
+TZ_DS_STRING_M T *tz_string_tolower(T *me)
 {
 	for (uint32_t i=0; i<me->n; ++i)
 		me->p[i] = tolower(me->p[i]);
 	return me;
 }
 
-TZ_STD_STRING_M T *tz_string_toupper_s(T *me, const char *s)
+TZ_DS_STRING_M T *tz_string_toupper_s(T *me, const char *s)
 {
 	size_t n = strlen(s);
 	tz_array_reserve(me, n);
@@ -110,18 +119,44 @@ TZ_STD_STRING_M T *tz_string_toupper_s(T *me, const char *s)
 	return me;
 }
 
-//TZ_STD_STRING_M T tz_string_tolower(T *me)
-//{
-//	//for (uint32_t i=0; i<me->n; ++i)
-//	//	me->p[i] = toupper(me->p[i]);
-//}
-//
-//TZ_STD_STRING_M T tz_string_capitalize(T *me)
-//{
-//	//if (me->n)
-//	//	me->p[0] = toupper(me->p[0]);
-//}
+/**
+ *
+ * return: array<string> */
+TZ_DS_STRING_M tz_array tz_string_split(char *line)
+{
+	tz_array ss = tz_array_create(sizeof(tz_string));
+	char *s=line;
 
-#endif /* TZ_STD_STRING_DECLARATIONS */
+	while (isspace(*s))
+		s++;
+
+	for (; *s; ++s) {
+		char *c=s;
+
+		if (*c == '"') {
+			s = ++c;
+			while (*s && *++s != '"')
+				;
+			if (*s == '\0')
+				return ss; // unterminated string
+			tz_string *x = tz_array_pushn(&ss, 1);
+			*x = tz_string_create_b(c, s-c);
+			continue;
+		}
+
+		if (!isspace(*s)) {
+			while (*s && !isspace(*++s))
+				;
+
+			//printf("[%.*s][%ld]\n", (int)(s-c), c, s-c);
+			tz_string *x = tz_array_pushn(&ss, 1);
+			*x = tz_string_create_b(c, s-c);
+			continue;
+		}
+	}
+	return ss;
+}
+
+#endif /* TZ_DS_STRING_DECLARATIONS */
 #undef T
-#endif /* TZ_STD_STRING_H */
+#endif /* TZ_DS_STRING_H */
