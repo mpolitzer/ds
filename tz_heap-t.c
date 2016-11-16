e97ac8b9fac6542abef48a55d93673953e525562
Marcelo Politzer Couto <mpolitzer.c@gmail.com>
2016-11-16 04:44:49 -0200

    (+) smoke test for heap

--- tz_heap-t.c
+++ tz_heap-t.c
@@ -1,5 +1,15 @@
+#include <limits.h>
+
+#define TZ_UNIT_TEST_INLINE
+#include "tz_unit_test.h"
+
 #define TZ_DS_HEAP_INLINE
 #include "tz_heap.h"
+
+bool int_gt(tz_heap *h, void *u, void *v)
+{
+	return *(int *)u > *(int *)v;
+}
 
 bool int_lt(tz_heap *h, void *u, void *v)
 {
@@ -8,23 +18,54 @@ bool int_lt(tz_heap *h, void *u, void *v)
 
 void test_smoke()
 {
-	tz_heap h = tz_heap_create(sizeof(int), int_lt);
+	{
+		tz_heap h = tz_heap_create(sizeof(int), int_lt);
 
-	tz_heap_put(&h, &(int){0});
-	tz_heap_put(&h, &(int){1});
-	tz_heap_put(&h, &(int){2});
-	tz_heap_put(&h, &(int){3});
+		tz_heap_put(&h, &(int){0});
+		tz_heap_put(&h, &(int){1});
+		tz_heap_put(&h, &(int){2});
+		tz_heap_put(&h, &(int){3});
 
-	while (tz_heap_length(&h)) {
-		printf("%d\n", *(int *)tz_heap_peek(&h));
-		tz_heap_pop(&h);
+		bool eq = true;
+		int prv = INT_MAX;
+		while (tz_heap_length(&h)) {
+			int i = *(int *)tz_heap_peek(&h);
+			eq &= prv >= i;
+			prv = i;
+
+			tz_heap_pop(&h);
+		}
+
+		tz_unit_test_should_pass(eq);
+		tz_heap_destroy(&h);
 	}
 
-	tz_heap_destroy(&h);
+	{
+		tz_heap h = tz_heap_create(sizeof(int), int_gt);
+
+		tz_heap_put(&h, &(int){0});
+		tz_heap_put(&h, &(int){1});
+		tz_heap_put(&h, &(int){2});
+		tz_heap_put(&h, &(int){3});
+
+		bool eq = true;
+		int prv = INT_MIN;
+		while (tz_heap_length(&h)) {
+			int i = *(int *)tz_heap_peek(&h);
+			eq &= prv <= i;
+			prv = i;
+
+			tz_heap_pop(&h);
+		}
+
+		tz_unit_test_should_pass(eq);
+		tz_heap_destroy(&h);
+	}
 }
 
 int main(int argc, char const* argv[])
 {
+	tz_unit_test_start(argv[0]);
 	test_smoke();
-	return 0;
+	return tz_unit_test_report();
 }
